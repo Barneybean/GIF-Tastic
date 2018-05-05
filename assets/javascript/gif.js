d1c19c444235b5b7dba3916b92275c9f96216d37
William <William@MacBook-Pro-2.local>
2018-05-05 05:58:17 -0700

    added click for dif in js and click for animation too.

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
         var showBtn = $("<button>");
         showBtn.html(predefinedBtn[i]);
         showBtn.attr("class","clickMe");
+        showBtn.attr("data-name", predefinedBtn[i]);
         $("#showBtn").prepend(showBtn);
     }
 
@@ -32,8 +33,9 @@ $(document).ready(function () {
 //*************grab API when topic button is clicked **************/
     $(document).on("click", ".clickMe",function (event) {
         event.preventDefault();
+        $("#showGif").empty();
         var dataName = $(this).attr("data-name");
-        var giphyUrl = "http://api.giphy.com/v1/gifs/search?q=" + dataName + "&api_key=o2KZbrj7FUzmnGfL1Yx5daKlI0quThpg&limit=20"
+        var giphyUrl = "http://api.giphy.com/v1/gifs/search?q="+dataName+"&api_key=o2KZbrj7FUzmnGfL1Yx5daKlI0quThpg&limit=20"
         $("#header").empty();
 
         $.ajax({
@@ -45,8 +47,35 @@ $(document).ready(function () {
             // var pageNum;
             var response = result.data;
             // var imageAnimate;
-            console.log(response.length);
-            
+            $("#header").text("Click Image for animation");
+
+            for (var a=0; a<response.length; a++) {
+                var gifStill = response[a].images.fixed_height_still.url;
+                var gifAnimate = response[a].images.fixed_height.url;
+                var gifDiv=$("<img>");
+                gifDiv.attr("src", gifStill);
+                gifDiv.attr("data-still", gifStill);
+                gifDiv.attr("data-animate", gifAnimate);
+                gifDiv.attr("class", "gifImage");
+                gifDiv.attr("data-state", "still");
+                $("#showGif").append(gifDiv);
+            }
+
+            $(".gifImage").on("click", function() {
+                var state = $(this).attr("data-state");
+                if (state == "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                }
+                else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            });
+        
+
+
+
             
 
 
